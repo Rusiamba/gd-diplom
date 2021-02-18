69434fb27cdf76e023e4920c94315474478ee6a9
Ruslan <rus.orudjev@mail.ru>
2021-02-18 22:33:46 +0300

    FV

---
+++ frontend/src/components/app/App.jsx
@@ -0,0 +1,10 @@
+﻿import React from "react";
+import NavBar from "./components/NavBar";
+
+export const App = () => {
+    return (
+      <div className='App'>
+        <NavBar />
+      </div>  
+    );
+};
